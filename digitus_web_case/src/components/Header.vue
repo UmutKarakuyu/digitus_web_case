34bb78b4df7193c7e825a33d23170695bf05665c
UmutKarakuyu <umutk@windowslive.com>
2024-03-31 15:50:14 +0300

    Initial Header Component

---
+++ digitus_web_case/src/components/Header.vue
@@ -0,0 +1,35 @@
+<script >
+
+</script>
+
+<template>
+  <div class="fixed w-full items-center justify-between flex flex-row bg-white">
+
+      <div class="flex justify-between items-center">
+        <img src="/assets/images/logo.png" alt="logo" class="h-16"/>
+      </div>
+      <div class="items-center justify-start space-x-2 sm:space-x-2 md:space-x-4 lg:space-x-8 xl:space-x-12 2xl:space-x-16 px-0 sm:px-0 md:px-4 lg:px-20 xl:px-40 2xl:px-80
+        hidden flex-row sm:flex md:flex lg:flex xl:flex 2xl:flex">
+        <img src="/assets/images/plus.png" alt="logo" class="h-16"/>
+          <h1>Moms</h1>
+          <h1>Experts</h1>
+          <h1>Explore</h1>
+          <h1>Forum</h1>
+          <h1>MomTalk</h1>
+      </div>
+      <div class="items-center justify-between flex flex-row">
+        <div class="hidden flex-row sm:hidden md:hidden lg:flex xl:flex 2xl:flex">
+          <img src="/assets/icons/search.svg" alt="logo" class="h-12"/>
+          <img src="/assets/icons/line.svg" alt="logo" class="h-12"/>
+          <img src="/assets/icons/inbox.svg" alt="logo" class="h-12"/>
+          <img src="/assets/icons/bell.svg" alt="logo" class="h-12"/>
+          <img src="/assets/icons/line.svg" alt="logo" class="h-12"/>
+        </div>
+        <div class="items-center justify-between flex flex-row">
+          <img src="/assets/images/profile_picture.png" alt="logo" class="h-12 object-cover rounded-full border-4 border-purple"/>
+          <img src="/assets/icons/arrow.svg" alt="logo" class="h-12"/>
+        </div>
+      </div>
+  </div>
+</template>
+
---
+++ digitus_web_case/src/views/Home.vue
@@ -0,0 +1,15 @@
+<script>
+import Header from '../components/Header.vue'
+
+export default {
+  components: {
+    Header,
+  }
+}
+</script>
+
+<template>
+  <div class="h-full w-full bg-purple-400">
+    <Header/>
+  </div>
+</template>
