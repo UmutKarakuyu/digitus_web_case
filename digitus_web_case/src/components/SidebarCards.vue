40c2e9953b1690605d722c17efdefb4bd681847b
UmutKarakuyu <umutk@windowslive.com>
2024-04-04 12:22:42 +0300

    Sidebar font size fix

--- digitus_web_case/src/components/SidebarCards.vue
+++ digitus_web_case/src/components/SidebarCards.vue
@@ -43,18 +43,18 @@ export default {
 <template>
     <div class=" flex flex-col space-y-12 ">
         <div v-for="item in cards" :key="item.id" class="bg-white" >
-            <div class=" flex flex-row space-x-4">
-                <div v-if="item.role === 'Medical Doctor'" class="items-center justify-center flex h-10 w-10 rounded-full border-transparent border-t-blue-500 border-b-yellow-500 bg-gradient-to-t from-yellow-500 via-green-500 to-blue-500">
-                    <img :src="item.avatar" alt="logo" class=" h-8 w-8 border-white border-4 rounded-full">
+            <div class=" flex flex-row space-x-2 p-1">
+                <div v-if="item.role === 'Medical Doctor'" class="items-center justify-center flex h-9 w-9 rounded-full border-transparent border-t-blue-500 border-b-yellow-500 bg-gradient-to-t from-yellow-500 via-green-500 to-blue-500">
+                    <img :src="item.avatar" alt="logo" class=" h-8 w-8 border-white border-2 rounded-full">
                 </div>
-                <div v-else-if="item.role === 'SPONSORED'" class="items-center justify-center flex h-10 w-10 rounded-full border-red-300 border-2">
-                    <img :src="item.avatar" alt="logo" class=" h-8 w-8 border-white border-4 rounded-full">
+                <div v-else-if="item.role === 'SPONSORED'" class="items-center justify-center flex h-9 w-9 rounded-full border-red-300 border-2">
+                    <img :src="item.avatar" alt="logo" class=" h-8 w-8 border-white border-2 rounded-full">
                 </div>
-                <div v-else class="items-center justify-center flex h-10 w-10 rounded-full border-purple-300 border-2">
-                    <img :src="item.avatar" alt="logo" class=" h-8 w-8 border-white border-4 rounded-full">
+                <div v-else class="items-center justify-center flex h-9 w-9 rounded-full border-purple-300 border-2">
+                    <img :src="item.avatar" alt="logo" class=" h-8 w-8 border-white border-2 rounded-full">
                 </div>
-                <div class="flex flex-col justify-center">
-                    <h1 class="text-xl">{{item.name}}</h1>
+                <div class="flex flex-col items-center justify-center">
+                    <h1 class="text-sm">{{item.name}}</h1>
                 </div>
             </div>
             <div class="flex flex-col w-full">
@@ -62,12 +62,17 @@ export default {
                     <div v-if="item.role !== 'SPONSORED'" class="h-1 w-full bg-gradient-to-r from-yellow-500 via-green-500 to-blue-500 border-transparent "></div>
                     <img v-if="item.pictures !== null" :src="item.pictures" alt="logo" class="w-full"/>
                 </div>
-                <div class="flex flex-col justify-between">
-                    <p class="font-bold text-xl p-4">{{item.description}}</p>
-                    <div class="flex flex-row justify-around">
-                        <h1 class="text-lg">{{ item.likeCount }} Likes</h1>
-                        
-                        <p>{{item.commentCount}} Comments</p>
+                <div class="flex flex-col justify-start">
+                    <p class="font-bold text-xs p-2">{{item.description}}</p>
+                    <div class="flex flex-row justify-around ">
+                        <div class=" flex flex-row space-x-1">
+                            <p class="text-xs">{{ item.likeCount }}</p>
+                            <p class="text-xs text-gray-400"> Likes</p>
+                        </div>
+                        <div class=" flex flex-row space-x-1">
+                            <p class="text-xs">{{item.commentCount}}</p>
+                            <p class="text-xs text-gray-400"> Comments</p>
+                        </div>
                     </div>
                     
                 </div>
